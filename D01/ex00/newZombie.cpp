28a554b9f26a02458544e5f031c204d5c23637cb
Nadege ALAVOINE <anadege@e3r10p19.clusters.42paris.fr>
2021-11-05 19:26:32 +0100

    retravaille

--- D01/ex00/newZombie.cpp
+++ D01/ex00/newZombie.cpp
@@ -4,6 +4,7 @@
 Zombie* newZombie(std::string name)
 {
     Zombie  *new_zombie;
+
     new_zombie = new Zombie(name);
     return new_zombie;
 } 
--- D01/ex00/randomChump.cpp
+++ D01/ex00/randomChump.cpp
@@ -3,6 +3,6 @@
 
 void    randomChump(std::string name)
 {
-    Zombie  new_zombie = Zombie(name);
+    Zombie  new_zombie(name);
     new_zombie.announce();
 }
