2e129f340fa25f955daf2ef7e836f925790d7dcd
Nadege ALAVOINE <anadege@e2r3p16.clusters.42paris.fr>
2021-12-15 12:33:42 +0100

    D07ex01 start

--- D07/ex01/main.cpp
+++ D07/ex01/main.cpp
@@ -1,29 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/15 12:15:08 by anadege           #+#    #+#             */
-/*   Updated: 2021/12/15 12:32:31 by anadege          ###   ########.fr       */
+/*   Created: 2021/12/15 12:30:20 by anadege           #+#    #+#             */
+/*   Updated: 2021/12/15 12:33:30 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __ITER_HPP__
-#define __ITER_HPP__
+#include "iter.hpp"
+#include <string>
+#include <iostream>
 
-template <typename T>
-T   test_fun(T)
+
+int main()
 {
-    return (T + 1);
+    int a[5] = {0, 1, 2, 3, 4};
+    for (int i = 0; i < 5; i++)
+        std::cout << a;
+    std::cout << "\n---> After iter" << std::endl;
+    
 }
-
-template <typename T>
-void    iter(T *arr, size_t arr_size, T (*function)(T))
-{
-    for (size_t i = 0; i < arr_size; i++)
-        T[i] = function(T[i]);
-}
-
-#endif
