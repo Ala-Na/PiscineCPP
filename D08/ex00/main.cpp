29463fe99fbd19d5a859cd60eaf16b2f8a0033d8
Nadege ALAVOINE <anadege@e2r5p16.clusters.42paris.fr>
2021-12-16 17:40:04 +0100

    d08ex00 done

--- D08/ex00/main.cpp
+++ D08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 14:08:02 by anadege           #+#    #+#             */
-/*   Updated: 2021/12/16 15:56:36 by anadege          ###   ########.fr       */
+/*   Updated: 2021/12/16 17:38:18 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 #include <list>
 #include <deque>
 #include <set>
-#include <stack>
 
 void    print(int elem)
 {
@@ -100,6 +99,33 @@ int main()
     try
     {
         std::deque<int>::iterator it = easyfind(dqu, 5);
+        std::cout << *it << ": \033[0;32mValue found in container\033[0m" << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "5: \033[0;31m" << e.what() << "\033[0m" << std::endl;
+    }
+
+    std::cout << "\n\033[0;35m---> Test for set\033[0m" << std::endl;
+    std::set<int> st;
+    st.insert(1);
+    st.insert(2);
+    st.insert(3);
+    std::cout << "Content: ";
+    for_each(st.begin(), st.end(), print);
+    std::cout << std::endl;
+    try
+    {
+        std::set<int>::iterator it = easyfind(st, 2);
+        std::cout << *it << ": \033[0;32mValue found in container\033[0m" << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "5: \033[0;31m" << e.what() << "\033[0m" << std::endl;
+    }
+    try
+    {
+        std::set<int>::iterator it = easyfind(st, 5);
         std::cout << *it << ": \033[0;32mValue found in container\033[0m" << std::endl;
     }
     catch (std::exception &e)
